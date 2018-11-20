6724893884a6a5c90e8a346fa888a846f8b646e1
CNBlackJ <yes.heng@icloud.com>
2018-11-20 10:22:25 +0800

    enhancement: isLoading

--- components/admin/productTable.vue
+++ components/admin/productTable.vue
@@ -90,7 +90,7 @@
 				this.$store.commit('uploadImgs/SET_IMGS', row.imgs)
 				this.$store.commit('uploadImgs/STE_DETAIL_IMGS', row.detailImgs.product)
 				if (!this.categories.length) await this.$store.dispatch('category/setCategories', { sort: 'index' })
-				this.$store.dispatch('product/setEditProduct')
+				await this.$store.dispatch('product/setEditProduct')
 				if(expandedRows.length > 1){
           expandedRows.shift()
         }
